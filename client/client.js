5584f2d959d7f3529a505c88b2d150f9b77e52b0
chan <bcchoi@deleo.co.kr>
2024-07-11 03:01:44 +0000

    [FIX] fix debug env parsing

--- client/client.js
+++ client/client.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const fetch = require('request-promise-native');
 const protocol = process.env.PROTOCOL || 'https';
 const ipAddress = process.env.IP_ADDRESS || '10.254.1.51';
@@ -5,15 +6,22 @@ const port = parseInt(process.env.PORT) || 443;
 const path = process.env.REQUEST_PATH || '/sleep';
 const host = process.env.HOST || 'modsecurity.test.dev'
 const count = parseInt(process.env.COUNT) || 1;
-const debug = process.env.DEBUG || false;
+const debug = process.env.DEBUG === 'true' ? true : false || false;
+let success = 0;
+let fail = 0;
 
-for (let i=0;i<count;i++) {
-  console.log(`[${i+1}] send request`);
-  sendRequest(i);
-}
+(async()=>{
+  for (let i=0;i<count;i++) {
+    console.log(`[${i+1}] send request`);
+    await sendRequest(i);
+  }
+  console.log(`success: ${success}
+fail: ${fail}`);
+})()
 
-function sendRequest(index) {
-  fetch({
+
+async function sendRequest(index) {
+  await fetch({
     uri: `${protocol}://${ipAddress}:${port}${path}`,
     method: 'GET',
     headers: {
@@ -25,8 +33,10 @@ function sendRequest(index) {
       console.log(res);
     }
     console.log(`[${index+1}] request ended..`)
+    success++;
   })
   .catch(err=>{
     console.error(err.error);
+    fail++;
   })
 }
